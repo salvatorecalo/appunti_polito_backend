9851d2055f3bae75fb1f5c54051a6d4fd26a4e53
salvo <dafder45678@gmail.com>
2026-05-26 21:37:46 +0200

    feat:  - add search for category and subcategory  - returns -1 in case of not category found  - returbs -5 in case of fetch error Signed-off-by: salvo <dafder45678@gmail.com>

--- index.js
+++ index.js
@@ -16,7 +16,6 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 /********* MongoDB Connection *********/
-
 const mongooseUrl = process.env.MONGO_URL;
 if (!mongooseUrl) {
   console.error("Error: MONGO_URL environment variable is not set.");
@@ -32,11 +31,62 @@ const CONFIG_PATH = path.join(__dirname, 'utils/cfg.json')
 const getConfig = () => JSON.parse(fs.readFileSync(CONFIG_PATH, 'utf-8'))
 const PORT = 3000
 
+
 app.get("/api/config", (req, res) => {
+    /*
+        Return the configuration file as JSON.
+        This files contains all the categories and subcategories.
+    */
+
     try {
         res.json(getConfig())
     } catch (e) {
         res.status(500).json({ error: "Failed to read config file" })
+    }
+})
+
+app.get('/api/get-list', async (req, res) => {
+    let cat = req.query.id
+    console.log(`Categoria ricevuta: ${cat}`)
+    const cfg = getConfig()
+    let defaultObj = {
+        status: 0,
+        subs: [],
+        ext: [],
+        int: []
+    }
+
+    if (cfg["courses"][cat] == null && cat != "gen"){
+        defaultObj.status = -1 // -1 stands for category not found
+        return res.json(defaultObj)
+    }
+
+    try {
+        if (cfg["subcats"] && cfg["subcats"][cat]){
+            defaultObjs.subs = cfg["subcats"][cat]
+        }
+
+        const dbLinks = await Link.find({cat: cat})
+
+        dbLinks.forEach(item => {
+            const formattedLink = {
+                link: item.link,
+                desc: item.desc,
+                sub: item.sub
+            }
+
+            if (item.is_ext){
+                defaultObj.ext.push(formattedLink)
+            } else {
+                defaultObj.int.push(formattedLink)
+            }
+        })
+
+        res.json(defaultObj)
+    } catch (e) {
+        console.error("Error during link fetch: ", err);
+        defaultObj.status = -5;
+        res.status(404).json(defaultObj)
     }
 })
 
